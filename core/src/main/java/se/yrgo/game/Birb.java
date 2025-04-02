4b00543207515318d6ccdf0b871fa21d130fe5ed
joakimgidlund <jockegidlund@gmail.com>
2025-04-02 11:09:40 +0200

    foundation for screen management. created base class where game starts and adapted the game screen to work.

---
+++ core/src/main/java/se/yrgo/game/Birb.java
@@ -0,0 +1,38 @@
+package se.yrgo.game;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+
+public class Birb extends Game {
+
+    public static final int SCREEN_WIDTH = 1280;
+    public static final int SCREEN_HEIGHT = 720;
+    
+    public SpriteBatch batch;
+    public BitmapFont font;
+    public FitViewport viewport;
+
+    public void create() {
+        Gdx.graphics.setWindowedMode(SCREEN_WIDTH, SCREEN_HEIGHT);
+        Gdx.graphics.setResizable(false);
+        viewport = new FitViewport(SCREEN_WIDTH, SCREEN_HEIGHT);
+
+        batch = new SpriteBatch();
+        font = new BitmapFont();
+        font.getData().setScale(2f);
+
+        this.setScreen(new GameScreen(this));
+    }
+
+    public void render() {
+        super.render();
+    }
+
+    public void dispose() {
+        batch.dispose();
+        font.dispose();
+    }
+}
