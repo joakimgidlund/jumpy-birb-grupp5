fe4af076f2a56a6035e31ffaf556fbfec1098ee7
joakimgidlund <jockegidlund@gmail.com>
2025-04-08 01:32:54 +0200

    basic menu created

--- core/src/main/java/se/yrgo/game/MainMenu.java
+++ core/src/main/java/se/yrgo/game/MainMenu.java
@@ -4,38 +4,90 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.badlogic.gdx.utils.ScreenUtils;
 
-public class MainMenu implements Screen {
+public class MainMenu implements Screen{
 
     Birb game;
+    Stage stage;
+    TextureAtlas atlas;
+    Skin skin;
+    Texture splash;
 
     public MainMenu(Birb game) {
         this.game = game;
+
+        atlas = new TextureAtlas("uiskin.atlas");
+        skin = new Skin(Gdx.files.internal("uiskin.json"), atlas);
+        splash = new Texture("doris.png");
+
+        stage = new Stage(game.viewport);
+        Gdx.input.setInputProcessor(stage);
+
+        game.viewport.apply();
     }
 
     @Override
     public void show() {
+        Table table = new Table();
+
+        table.setFillParent(true);
+        table.top();
+
+        TextButton play = new TextButton("Play", skin);
+        TextButton quit = new TextButton("Quit", skin);
+        
+        play.addListener(new ChangeListener() {
+            @Override
+            public void changed(ChangeEvent event, Actor actor) {
+                game.setScreen(new GameScreen(game));
+            }
+        });
+
+        quit.addListener(new ChangeListener() {
+            @Override
+            public void changed(ChangeEvent event, Actor actor) {
+                Gdx.app.exit();
+            }
+        });
+
+        table.padTop(400);
+        table.add(play).height(100).width(200);
+        table.row();
+        table.add(quit).height(100).width(200);
+        table.row();
+
+        stage.addActor(table);
     }
 
     @Override
     public void render(float delta) {
         ScreenUtils.clear(Color.BLACK);
 
-        game.viewport.apply();
-
         game.batch.begin();
-        game.font.draw(game.batch, "Press N to start.", Birb.SCREEN_WIDTH / 2, Birb.SCREEN_HEIGHT / 2);
+        // game.font.draw(game.batch, "Press N to start.", Birb.SCREEN_WIDTH / 2, Birb.SCREEN_HEIGHT / 2);
+        game.batch.draw(splash, game.SCREEN_WIDTH / 2 - 25, 600);
         game.batch.end();
+        
+        stage.act();
+        stage.draw();
 
-        if (Gdx.input.isKeyJustPressed(Input.Keys.N) || Gdx.input.isButtonJustPressed(Input.Buttons.RIGHT)) {
-            game.setScreen(new GameScreen(game));
-            dispose();
-        }
+        // if (Gdx.input.isKeyJustPressed(Input.Keys.N) || Gdx.input.isButtonJustPressed(Input.Buttons.RIGHT)) {
+        //     game.setScreen(new GameScreen(game));
+        //     dispose();
+        // }
 
-        if(Gdx.input.isKeyJustPressed(Input.Keys.ESCAPE)) {
-            Gdx.app.exit();
-        }
+        // if(Gdx.input.isKeyJustPressed(Input.Keys.ESCAPE)) {
+        //     Gdx.app.exit();
+        // }
     }
 
     @Override
@@ -57,5 +109,7 @@ public class MainMenu implements Screen {
 
     @Override
     public void dispose() {
+        skin.dispose();
+        atlas.dispose();
     }
 }
