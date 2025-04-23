d8b5fb076f37a2cf8348ee50022e403a41f192cb
joakimgidlund <jockegidlund@gmail.com>
2025-04-23 12:33:09 +0200

    import BitmapFont

--- core/src/main/java/se/yrgo/game/GameScreen.java
+++ core/src/main/java/se/yrgo/game/GameScreen.java
@@ -7,6 +7,7 @@ import java.util.concurrent.ThreadLocalRandom;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.scenes.scene2d.Stage;
